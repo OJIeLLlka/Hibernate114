6bc4b801df70a554b1d7cae4f023c2b3c4ff2941
Oleg Belov <91963011+OJIeLLlka@users.noreply.github.com>
2022-05-30 13:28:45 +0300

    Update Util.java
    fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,18 +17,10 @@ import java.util.Properties;
 public class Util {
     // реализуйте настройку соеденения с БД
 
-//    private static Util util;
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mydatabase";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
     private static SessionFactory sessionFactory;
-
-//    public static synchronized Util getUtil() {
-//        if (util == null) {
-//            util = new Util();
-//        }
-//        return util;
-//    }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
